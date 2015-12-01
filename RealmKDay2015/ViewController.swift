12d85ad61506e12b1f9d2dc45ed5a03392617cb8
Petar Mataic <petar@Petars-MacBook-Pro.local>
2015-12-01 11:32:29 +0100

    Initial commit and added Realm framework

---
+++ RealmKDay2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmKDay2015
+//
+//  Created by Petar Mataic on 01/12/15.
+//  Copyright © 2015 Petar Mataic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
